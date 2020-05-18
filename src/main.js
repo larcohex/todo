c0ec2c221e61662998edbc487257a83acbd6d9dd
larcohex <kudayar.pirimbaev@gmail.com>
2020-05-19 03:10:24 +0600

    0.0.1
    * Add TODO app

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 import Vue from "vue";
+import localForage from "localforage";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCaretDown,
+  faObjectGroup,
+  faPen,
+  faPlus,
+  faTimes
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
+
+library.add(faCaretDown, faObjectGroup, faPen, faPlus, faTimes);
+Vue.component("fa-icon", FontAwesomeIcon);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  beforeCreate() {
+    localForage.getItem("todo-state").then(state => {
+      store.state.tasks = state.tasks;
+      store.state.groups = state.groups;
+      store.state.init = true;
+    });
+  }
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,63 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { v4 as uuidv4 } from "uuid";
+import localForage from "localforage";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    tasks: [],
+    groups: [],
+    init: false
+  },
+  actions: {
+    addTask({ state }, newTask) {
+      state.tasks.push({ ...newTask, id: uuidv4() });
+      localForage.setItem("todo-state", state);
+    },
+    updateTask({ state }, task) {
+      for (let i = 0; i < state.tasks.length; ++i) {
+        if (state.tasks[i].id === task.id) {
+          state.tasks[i].title = task.title;
+          state.tasks[i].description = task.description;
+          state.tasks[i].done = task.done;
+          state.tasks[i].group = task.group;
+          break;
+        }
+      }
+      localForage.setItem("todo-state", state);
+    },
+    removeTask({ state }, taskId) {
+      for (let i = 0; i < state.tasks.length; ++i) {
+        if (state.tasks[i].id === taskId) {
+          state.tasks.splice(i, 1);
+          break;
+        }
+      }
+      localForage.setItem("todo-state", state);
+    },
+    addGroup({ state }, newGroup) {
+      state.groups.push({ ...newGroup, id: uuidv4() });
+      localForage.setItem("todo-state", state);
+    },
+    updateGroup({ state }, group) {
+      for (let i = 0; i < state.groups.length; ++i) {
+        if (state.groups[i].id === group.id) {
+          state.groups[i].title = group.title;
+          break;
+        }
+      }
+      localForage.setItem("todo-state", state);
+    },
+    removeGroup({ state }, groupId) {
+      for (let i = 0; i < state.groups.length; ++i) {
+        if (state.groups[i].id === groupId) {
+          state.groups.splice(i, 1);
+          break;
+        }
+      }
+      localForage.setItem("todo-state", state);
+    }
+  }
 });
